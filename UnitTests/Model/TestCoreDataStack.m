20e2faaa7a2dca09e63de71319f4ae7cfc2d81c8
Adam Axe <adam@teamaxe.org>
2012-05-12 22:21:28 -0500

    Added project settings for code coverage and class to fix Xcode bug

--- UnitTests/Model/TestCoreDataStack.m
+++ UnitTests/Model/TestCoreDataStack.m
@@ -96,3 +96,15 @@
     [super dealloc];
 }
 @end
+
+#include <stdio.h>
+// Prototype declarations
+FILE *fopen$UNIX2003( const char *filename, const char *mode );
+size_t fwrite$UNIX2003( const void *a, size_t b, size_t c, FILE *d );
+
+FILE *fopen$UNIX2003( const char *filename, const char *mode ) {
+    return fopen(filename, mode);
+}
+size_t fwrite$UNIX2003( const void *a, size_t b, size_t c, FILE *d ) {
+    return fwrite(a, b, c, d);
+}
