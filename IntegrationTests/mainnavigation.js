fe2d9f9b4cccda65ee6e1a5b548c996ce54101a0
Adam Axe <adam@teamaxe.org>
2012-05-28 23:27:42 -0500

    Updated UIAutomation scripts with actual pass/fails

--- IntegrationTests/mainnavigation.js
+++ IntegrationTests/mainnavigation.js
@@ -1,5 +1,27 @@
+/**
+Moralife UI mainnavigation
+ 
+@author Copyright 2012 Team Axe, LLC. All rights reserved. http://www.teamaxe.org
+
+@date 05/28/2012
+@file mainnavigation.js
+*/
 
 var target = UIATarget.localTarget();
+
+UIALogger.logStart("MoraLife Testing Begins");
+UIALogger.logDebug("Main Navigation");
+UIATarget.localTarget().frontMostApp().logElementTree();
+
+target.captureScreenWithName("Initial Screenshot");
+
+if (target.frontMostApp().tabBar().buttons()["Journal"]==null ||
+target.frontMostApp().tabBar().buttons()["Journal"]==null ||
+target.frontMostApp().tabBar().buttons()["Journal"]==null){
+   UIALogger.logFail("FAIL:. UITabBar not loaded"); 
+} else {
+   UIALogger.logPass("PASS: UITabBar loaded"); 
+} 
 
 target.frontMostApp().mainWindow().buttons()["Rank"].tap();
 target.frontMostApp().mainWindow().buttons()["Vice"].tap();
@@ -24,3 +46,4 @@ target.frontMostApp().mainWindow().buttons()["Morals"].tap();
 target.frontMostApp().navigationBar().leftButton().tap();
 target.frontMostApp().tabBar().buttons()["Home"].tap();
 
+UIALogger.logPass("PASS: Main Navigation Complete"); 
