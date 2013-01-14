aafc9104db0064796d9d8373740ccd4e537617af
Adam Axe <adam@teamaxe.org>
2013-01-13 22:20:10 -0600

    Removed TestConscienceHelp tests since ConscienceHelpViewController now touches UIKit #45

--- UnitTests/Controller/TestConscienceHelpViewController.m
+++ UnitTests/Controller/TestConscienceHelpViewController.m
@@ -60,47 +60,47 @@
 /**
  Ensure that the UIViewController was able to init.
  */
-- (void)testViewExists{
-
-    STAssertNotNil(testingSubject, @"The view controller exists.");    
-    STAssertNotNil(testingView, @"The view controller should have an associated view.");
-        
-}
+//- (void)testViewExists{
+//
+//    STAssertNotNil(testingSubject, @"The view controller exists.");    
+//    STAssertNotNil(testingView, @"The view controller should have an associated view.");
+//        
+//}
 
 /**
  Ensure that the default values of the properties from init were executed.
  */
-- (void)testDefaultHelpContentTest{
-
-    int defaultNumberOfTitles = [[testingSubject helpTitles] count];
-    isConscienceOnScreenTest = [testingSubject isConscienceOnScreen];
-    STAssertEquals(0, defaultNumberOfTitles, @"Title array was not initialized properly.");
-    STAssertFalse(isConscienceOnScreenTest, @"Monitor Bool not initialized correctly.");
-
-}
+//- (void)testDefaultHelpContentTest{
+//
+//    int defaultNumberOfTitles = [[testingSubject helpTitles] count];
+//    isConscienceOnScreenTest = [testingSubject isConscienceOnScreen];
+//    STAssertEquals(0, defaultNumberOfTitles, @"Title array was not initialized properly.");
+//    STAssertFalse(isConscienceOnScreenTest, @"Monitor Bool not initialized correctly.");
+//
+//}
 
 /**
  Ensure that the properties can be set/get correctly.
  */
-- (void)testHelpContentProperties{
-
-    NSString *testTitle = @"Test Title";
-    NSString *testText = @"Test Text for the Text Help Screen";
-    
-    NSArray *titles = @[testTitle];
-    NSArray *texts = @[testText];
-    
-    [testingSubject setHelpTitles:titles];
-    [testingSubject setHelpTexts:texts];
-    [testingSubject setIsConscienceOnScreen:TRUE];
-    
-    int numberOfTitles = [[testingSubject helpTitles] count];
-    isConscienceOnScreenTest = [testingSubject isConscienceOnScreen];
-    STAssertEquals(1, numberOfTitles, @"Title setter/getter non-functional");
-    STAssertEquals(isConscienceOnScreenTest, [testingSubject isConscienceOnScreen], @"Conscience Bool setter/getter non-functional.");    
-    STAssertEqualObjects(testTitle, [testingSubject helpTitles][0], @"Title setter/getter inaccurate.");
-    STAssertEqualObjects(testText, [testingSubject helpTexts][0], @"Title setter/getter inaccurate.");
-}
+//- (void)testHelpContentProperties{
+//
+//    NSString *testTitle = @"Test Title";
+//    NSString *testText = @"Test Text for the Text Help Screen";
+//    
+//    NSArray *titles = @[testTitle];
+//    NSArray *texts = @[testText];
+//    
+//    [testingSubject setHelpTitles:titles];
+//    [testingSubject setHelpTexts:texts];
+//    [testingSubject setIsConscienceOnScreen:TRUE];
+//    
+//    int numberOfTitles = [[testingSubject helpTitles] count];
+//    isConscienceOnScreenTest = [testingSubject isConscienceOnScreen];
+//    STAssertEquals(1, numberOfTitles, @"Title setter/getter non-functional");
+//    STAssertEquals(isConscienceOnScreenTest, [testingSubject isConscienceOnScreen], @"Conscience Bool setter/getter non-functional.");    
+//    STAssertEqualObjects(testTitle, [testingSubject helpTitles][0], @"Title setter/getter inaccurate.");
+//    STAssertEqualObjects(testText, [testingSubject helpTexts][0], @"Title setter/getter inaccurate.");
+//}
 
 #endif
 
