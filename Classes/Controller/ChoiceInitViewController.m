47d47d9aeab3ae619733e87098aa701b811eaeb1
Adam Axe <adam@teamaxe.org>
2013-02-23 14:51:14 -0600

    Frames around buttons on menu screens #84

--- Classes/Controller/ChoiceInitViewController.m
+++ Classes/Controller/ChoiceInitViewController.m
@@ -22,16 +22,15 @@ Implementation:  UIViewController allows subsequent screen selection, controls b
     IBOutlet UIView *badChoiceView;                /**< Container view for Good Choice */
     IBOutlet UIView *choiceListView;                /**< Container view for Good Choice */
 
-	IBOutlet UIButton *goodChoiceLabelButton;		/**< Label for Good Choice entry selection */
-	IBOutlet UIButton *badChoiceLabelButton;		/**< Label for Bad Choice entry selection */
-	IBOutlet UIButton *choiceListLabelButton;		/**< Label for All Choice listing selection */
+	IBOutlet UILabel *goodChoiceLabel;		/**< Label for Good Choice entry selection */
+	IBOutlet UILabel *badChoiceLabel;		/**< Label for Bad Choice entry selection */
+	IBOutlet UILabel *choiceListLabel;		/**< Label for All Choice listing selection */
 	
 	IBOutlet UIButton *goodChoiceButton;		/**< Button for Good Choice entry selection */
 	IBOutlet UIButton *badChoiceButton;			/**< Button for Bad Choice entry selection */
 	IBOutlet UIButton *choiceListButton;		/**< Button for All Choice listing selection */
 	    
 	NSArray *buttonNames;		/**< button image names */
-	NSTimer *buttonTimer;		/**< Timer for animating buttons */
 }
 
 @end
@@ -59,21 +58,11 @@ Implementation:  UIViewController allows subsequent screen selection, controls b
 - (void)viewDidLoad {
 	[super viewDidLoad];
 
-    goodChoiceView.alpha = 0;
-    badChoiceView.alpha = 0;
-    choiceListView.alpha = 0;
+    goodChoiceLabel.font = [UIFont fontForHomeScreenButtons];
+    badChoiceLabel.font = [UIFont fontForHomeScreenButtons];
+    choiceListLabel.font = [UIFont fontForHomeScreenButtons];
 
     self.navigationItem.hidesBackButton = YES;
-    goodChoiceLabelButton.titleLabel.font = [UIFont fontForScreenButtons];
-    [goodChoiceLabelButton setTitleShadowColor:[UIColor whiteColor] forState:UIControlStateNormal];
-    [goodChoiceLabelButton.titleLabel setShadowOffset:CGSizeMake(0, 1)];
-    badChoiceLabelButton.titleLabel.font = [UIFont fontForScreenButtons];
-    [badChoiceLabelButton setTitleShadowColor:[UIColor whiteColor] forState:UIControlStateNormal];
-    [badChoiceLabelButton.titleLabel setShadowOffset:CGSizeMake(0, 1)];
-    choiceListLabelButton.titleLabel.font = [UIFont fontForScreenButtons];
-    [choiceListLabelButton setTitleShadowColor:[UIColor whiteColor] forState:UIControlStateNormal];
-    [choiceListLabelButton.titleLabel setShadowOffset:CGSizeMake(0, 1)];
-
 
     UIBarButtonItem *choiceBarButton = [[UIBarButtonItem alloc] initWithTitle:@"Home" style:UIBarButtonItemStylePlain target:self.navigationController action:@selector(popToRootViewControllerAnimated:)];
     [self.navigationItem setLeftBarButtonItem:choiceBarButton];
@@ -85,22 +74,11 @@ Implementation:  UIViewController allows subsequent screen selection, controls b
 
     [super viewWillAppear:animated];
 
-	//Setup repeating NSTimer to make the buttons animate randomly
-	[self refreshButtons];
-	buttonTimer = [NSTimer scheduledTimerWithTimeInterval:2 target:self selector:@selector(refreshButtons) userInfo:nil repeats:YES];
-
 }
 
 -(void)viewDidAppear:(BOOL)animated {
     
     [super viewDidAppear:animated];
-
-    [UIView animateWithDuration:0.35 animations:^{
-
-        goodChoiceView.alpha = 1.0;
-        badChoiceView.alpha = 1.0;
-        choiceListView.alpha = 1.0;
-    }];
 
     [NSTimer scheduledTimerWithTimeInterval:0.0 target:self selector:@selector(showInitialHelpScreen) userInfo:nil repeats:NO];
 
@@ -203,77 +181,18 @@ Implementation: A single view controller is utilized for both Good and Bad choic
 	self.accessibilityHint = NSLocalizedString(@"ChoiceInitScreenHint",nil);
     
 	//Set localized Button Titles.  Do no do this in init, XIB is not loaded until viewDidLoad
-	[goodChoiceLabelButton setTitle:NSLocalizedString(@"ChoiceInitScreenChoiceGoodLabel",nil) forState: UIControlStateNormal];
-	[badChoiceLabelButton setTitle:NSLocalizedString(@"ChoiceInitScreenChoiceBadLabel",nil) forState: UIControlStateNormal];
-	[choiceListLabelButton setTitle:NSLocalizedString(@"ChoiceInitScreenChoiceListLabel",nil) forState: UIControlStateNormal];
-	goodChoiceLabelButton.accessibilityHint = NSLocalizedString(@"ChoiceInitScreenChoiceGoodHint",nil);
-	badChoiceLabelButton.accessibilityHint = NSLocalizedString(@"ChoiceInitScreenChoiceBadHint",nil);
-	choiceListLabelButton.accessibilityHint = NSLocalizedString(@"ChoiceInitScreenChoiceListHint",nil);		
+	goodChoiceLabel.text = NSLocalizedString(@"ChoiceInitScreenChoiceGoodLabel",nil);
+	badChoiceLabel.text = NSLocalizedString(@"ChoiceInitScreenChoiceBadLabel",nil);
+	choiceListLabel.text = NSLocalizedString(@"ChoiceInitScreenChoiceListLabel",nil);
+	goodChoiceLabel.accessibilityHint = NSLocalizedString(@"ChoiceInitScreenChoiceGoodHint",nil);
+	badChoiceLabel.accessibilityHint = NSLocalizedString(@"ChoiceInitScreenChoiceBadHint",nil);
+	choiceListLabel.accessibilityHint = NSLocalizedString(@"ChoiceInitScreenChoiceListHint",nil);		
 	goodChoiceButton.accessibilityLabel = NSLocalizedString(@"ChoiceInitScreenChoiceGoodLabel",nil);
 	badChoiceButton.accessibilityLabel = NSLocalizedString(@"ChoiceInitScreenChoiceBadLabel",nil);
 	choiceListButton.accessibilityLabel = NSLocalizedString(@"ChoiceInitScreenChoiceListLabel",nil);
 	goodChoiceButton.accessibilityHint = NSLocalizedString(@"ChoiceInitScreenChoiceGoodHint",nil);
 	badChoiceButton.accessibilityHint = NSLocalizedString(@"ChoiceInitScreenChoiceBadHint",nil);
 	choiceListButton.accessibilityHint = NSLocalizedString(@"ChoiceInitScreenChoiceListHint",nil);
-	
-}
-
-
-#pragma mark -
-#pragma mark MenuScreenAnimations Protocol
-
-/**
- Implementation: Only animate at most 4 buttons at a time.  Otherwise, too visually distracting
- */
-- (void) refreshButtons{
-
-    //Determine which set of four buttons will be animated
-	[self buttonAnimate:@(arc4random()%3)];
-	[self buttonAnimate:@(arc4random()%3)];
-	[self buttonAnimate:@(arc4random()%3)];
-	[self buttonAnimate:@(arc4random()%3)];
-}
-
-/**
- Implementation: Build animated UIImage from sequential icon files
- */
-- (void) buttonAnimate:(NSNumber *) buttonNumber{
-	
-    //Get button image base filename
-	NSString *iconFileName = [NSString stringWithFormat:@"icon-%@ani", buttonNames[[buttonNumber intValue]]];
-
-    //Build 4 frames of animation for selected button
-	UIImage *iconani1 = [UIImage imageNamed:[NSString stringWithFormat:@"%@1.png", iconFileName]];
-	UIImage *iconani2 = [UIImage imageNamed:[NSString stringWithFormat:@"%@2.png", iconFileName]];
-	UIImage *iconani3 = [UIImage imageNamed:[NSString stringWithFormat:@"%@3.png", iconFileName]];
-	UIImage *iconani4 = [UIImage imageNamed:[NSString stringWithFormat:@"%@4.png", iconFileName]];
-
-    //Setup placeholder for image setup
-	UIImage *iconBlank = [UIImage imageNamed:@""];
-	
-    //Build physical animation in UIImageView
-	UIImageView *animation1 = [[UIImageView alloc] init];
-	animation1.frame = CGRectMake(0,0,75,75);
-	
-	NSArray *images = @[iconani1, iconani2, iconani3, iconani4];
-
-	animation1.animationImages = images;
-    
-    //Determine which button to animate
-	switch ([buttonNumber intValue]){
-		case 0: [goodChoiceButton setBackgroundImage:iconBlank forState:UIControlStateNormal];[goodChoiceButton addSubview:animation1];break;
-		case 1: [badChoiceButton setBackgroundImage:iconBlank forState:UIControlStateNormal];[badChoiceButton addSubview:animation1];break;
-		case 2: [choiceListButton setBackgroundImage:iconBlank forState:UIControlStateNormal];[choiceListButton addSubview:animation1];break;
-		default:break;
-	}
-	
-	animation1.animationDuration = 0.75;
-	animation1.animationRepeatCount = 1;
-	[animation1 startAnimating];
-	
-	
-	//Reset button after animation, delay slightly to allow animation to catchup
-	[self performSelector:@selector(buttonAnimationDone:) withObject:buttonNumber afterDelay:0.70];
 	
 }
 
@@ -296,14 +215,6 @@ Implementation: A single view controller is utilized for both Good and Bad choic
 #pragma mark -
 #pragma mark Memory Management
 
--(void) viewWillDisappear:(BOOL)animated {
-
-	//Turn off button animations
-	[buttonTimer invalidate];
-	buttonTimer = nil;
-	
-}
-
 - (void)didReceiveMemoryWarning {
     // Releases the view if it doesn't have a superview.
     [super didReceiveMemoryWarning];
@@ -313,7 +224,5 @@ Implementation: A single view controller is utilized for both Good and Bad choic
 - (void)viewDidUnload {
     [super viewDidUnload];
 }
-
-
 
 @end
