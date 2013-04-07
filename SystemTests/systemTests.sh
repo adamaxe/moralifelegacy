d217c864d16227e0602c18b153ba0893b00526d6
Adam Axe <adam@teamaxe.org>
2013-04-07 14:04:01 -0500

    updated systemTests script to allow for local or CI box builds #104

--- SystemTests/systemTests.sh
+++ SystemTests/systemTests.sh
@@ -1,29 +1,49 @@
 #! /bin/sh
-#Wipe the simulator
-#rm -Rf ~/Library/Application\ Support/iPhone\ Simulator/
-
-mkdir ~/Downloads/results
 
 #Setup script order
 scripts=( "introcompletion" "helpscreendismissals" "referenceverify" "reportpienavigation" "choicemoralentry" "choicemoralverify" "mainnavigation" "consciencenavigation" "choicemoraldisplay" "choicemoralcancel" "choicemoralentry" "choicemoralverify"
 "choiceimmoraldisplay" "choiceimmoralcancel" "choiceimmoralentry" "choiceimmoralverify")
 
-for i in "${scripts[@]}"
-do
 
-#Remove previous trace files and reports
-#rm -Rf *.trace
-#rm -rf ${WORKSPACE}/SystemTests/results
-#mkdir ${WORKSPACE}/SystemTests/results
+#Determine if script is being run from workstation or CI box
+if [ -z "$JENKINS_SYSTEM_TESTS" ]; then
 
-#Launch Instruments, use UIAutomation template, point to currently-built app, run js test script, send output to results dir
-#instruments -t /Applications/Xcode.app/Contents/Applications/Instruments.app/Contents/PlugIns/AutomationInstrument.bundle/Contents/Resources/Automation.tracetemplate ${WORKSPACE}/build/Release-iphonesimulator/MoraLife.app -e UIASCRIPT ${WORKSPACE}/SystemTests/$i.js -e UIARESULTSPATH ${WORKSPACE}/SystemTests/results
+    #Wipe the simulator
+    rm -Rf ~/Library/Application\ Support/iPhone\ Simulator/
 
-#Copy the OCUnit results xml to reports
-#cp "${WORKSPACE}/SystemTests/results/Run 1/Automation Results.plist" ${WORKSPACE}/test-reports/$i.xml
+    #Iterate through every script
+    for i in "${scripts[@]}"
+    do
 
-instruments -t /Applications/Xcode.app/Contents/Applications/Instruments.app/Contents/PlugIns/AutomationInstrument.bundle/Contents/Resources/Automation.tracetemplate $BUILT_PRODUCTS_DIR/$WRAPPER_NAME -e UIASCRIPT $SRCROOT/SystemTests/$i.js -e UIARESULTSPATH ~/Downloads/results
+        #Remove previous trace files and reports
+        rm -Rf *.trace
+        rm -rf ${WORKSPACE}/SystemTests/results
+        mkdir ${WORKSPACE}/SystemTests/results
 
-done
+        #Launch Instruments, use UIAutomation template, point to currently-built app, run js test script, send output to results dir
+        instruments -t /Applications/Xcode.app/Contents/Applications/Instruments.app/Contents/PlugIns/AutomationInstrument.bundle/Contents/Resources/Automation.tracetemplate ${WORKSPACE}/build/Release-iphonesimulator/MoraLife.app -e UIASCRIPT ${WORKSPACE}/SystemTests/$i.js -e UIARESULTSPATH ${WORKSPACE}/SystemTests/results
 
-rm -Rf ~/Downloads/results
+        #Copy the OCUnit results xml to reports
+        cp "${WORKSPACE}/SystemTests/results/Run 1/Automation Results.plist" ${WORKSPACE}/test-reports/$i.xml
+
+    done
+
+    exit 1
+else
+
+    #Setup a place for the results to be copied to
+    RESULTS_DIRECTORY=~/Downloads/results
+    mkdir $RESULTS_DIRECTORY
+
+    #Iterate through every script
+    for i in "${scripts[@]}"
+    do
+
+        instruments -t /Applications/Xcode.app/Contents/Applications/Instruments.app/Contents/PlugIns/AutomationInstrument.bundle/Contents/Resources/Automation.tracetemplate $BUILT_PRODUCTS_DIR/$WRAPPER_NAME -e UIASCRIPT $SRCROOT/SystemTests/$i.js -e UIARESULTSPATH $RESULTS_DIRECTORY
+
+    done
+
+    rm -Rf $RESULTS_DIRECTORY
+    exit 1
+
+fi
