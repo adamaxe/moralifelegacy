6e4c0a2dc4ab79211fcf2ca8d1f8f247b9062f1d
Adam Axe <adam@adamaxe.com>
2016-07-03 12:55:54 -0500

    fixed accuracy on conscience tests

--- UnitTests/Model/TestConscienceBody.m
+++ UnitTests/Model/TestConscienceBody.m
@@ -90,8 +90,8 @@
     [testingSubject setAge:testAge];
     [testingSubject setSize:testSize];
     
-    XCTAssertEqual(testSize, [testingSubject size], @"size setter/getter inaccurate.");
-	XCTAssertEqual(testAge, [testingSubject age], @"age setter/getter inaccurate.");
+    XCTAssertEqualWithAccuracy(testSize, testingSubject.size, 1, @"size setter/getter inaccurate.");
+    XCTAssertEqualWithAccuracy(testAge, testingSubject.age, @"age setter/getter inaccurate.");
 	XCTAssertEqual(testBubbleType, [testingSubject bubbleType], @"bubbleType setter/getter inaccurate.");
 	XCTAssertEqualObjects(testEyeFileName, [testingSubject eyeName], @"eyeName setter/getter inaccurate.");
 	XCTAssertEqualObjects(testMouthFileName, [testingSubject mouthName], @"mouthName setter/getter inaccurate.");
